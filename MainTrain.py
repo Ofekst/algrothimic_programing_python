1938ce8275fb5f402d7df353c74d2b366518f13d
Ofekst <Ofekstrteicher0@gmail.com>
2025-06-09 18:15:33 +0300

    Done with  exercise 5

--- MainTrain.py
+++ MainTrain.py
@@ -1,41 +1,41 @@
-
-import datetime
+from tsp_ga import *
 import random
-from allSums import *
+import sys
 
-def getAllSums(arr):
-
-    ret = set()
-
-    def allSums(sum ,index):
-        if index==len(arr):
-            ret.add(sum)
-            return
-        allSums(sum +arr[index] ,index +1)
-        allSums(sum ,index +1)
-
-    allSums(0 ,0)
-    return ret
+class Point:
+    def __init__(self) -> None:
+        self.x = random.randint(-100,100)
+        self.y = random.randint(-100,100)
 
 
+def dist(p1,p2):
+    return float(math.sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2))
 
-arr =[random.randint(1, 10) for x in range(20)]
+def fitness(points):
+    sum=0
+    for i in range(1,len(points)):
+        sum+=dist(points[i-1],points[i])
+    return sum
 
-t0 = datetime.datetime.now()
-result = getAllSums(arr)
-t1 = datetime.datetime.now()
-delta = t1 - t0
 
-t0 = datetime.datetime.now()
-result2 = allSumsDP(arr)
-t1 = datetime.datetime.now()
-delta2 = t1 - t0
+def randSol(points,size):
+    mn=sys.maxsize
+    for i in range(size):
+        ps = points.copy()
+        random.shuffle(ps)
+        f = fitness(ps)
+        if f<mn:
+            mn=f
+            best=ps
+    return best
 
-if result != result2:
-    print(f"your solution is incorrect (-50)")
-if delta2.microseconds * 100 >= delta.microseconds:
-    print("your solution is not efficent enough (-50)")
+
+for i in range(5):
+    print(f"test {i}")
+    points=[Point() for i in range(100)]
+    rs = fitness(randSol(points,20))
+    gas = fitness(solve(points))
+    if gas>rs:
+        print("your GA did not have a better result than the random algo (-20)")
 
 print("done")
-
-
---
+++ tsp_ga.py
@@ -0,0 +1,53 @@
+import math
+
+
+def calculate_euclidean_distance(p1, p2) -> float:
+    """
+    Calculates the Euclidean distance between two points in 2D space.
+
+    :param p1: The first point
+    :param p2: The second point
+    :return: The Euclidean distance between p1 and p2 as float
+    """
+    return math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2)
+
+def find_closest_point(current_point, points:list, visited:set[int]):
+    """
+    Finds the closest unvisited point in the list of points.
+    :param current_point: The point from which distances are calculated
+    :param points: List of all points
+    :param visited: Set of indices of already visited points
+    :return: The closest unvisited point and its index
+    """
+    min_distance = float("inf")
+    closest_point = None
+    closest_index = -1
+
+    for i, point in enumerate(points):
+        if i not in visited:
+            distance = calculate_euclidean_distance(current_point, point)
+            if distance < min_distance:
+                min_distance = distance
+                closest_point = point
+                closest_index = i
+
+    return closest_point, closest_index
+
+def solve(points):
+
+    visited_points = set()
+    best_path = [points[0]]
+    visited_points.add(0)
+
+    current_point = points[0]
+
+    while len(visited_points) < len(points):
+        closest_point, closest_index = find_closest_point(current_point, points, visited_points)
+        if closest_point:
+            best_path.append(closest_point)
+            visited_points.add(closest_index)
+            current_point = closest_point
+
+    best_path.append(points[0])
+
+    return best_path
